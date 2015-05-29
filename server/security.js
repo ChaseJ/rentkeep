2b5593044937e992885455398b78ea99c9e6010c
Chase Greiner <chase.greiner@gmail.com>
2015-05-28 21:05:30 -0500

    Added edit property modal

--- server/security.js
+++ server/security.js
@@ -1,1 +1,11 @@
+//Method to check whether it's the current user's document
+Security.defineMethod("ifIsCurrentUser", {
+    fetch: [],
+    transform: null,
+    deny: function (type, arg, userId, doc) {
+        return userId !== doc.userId;
+    }
+});
+
 Properties.permit('insert').ifLoggedIn().apply();
+Properties.permit(['update', 'remove']).ifIsCurrentUser().apply();
