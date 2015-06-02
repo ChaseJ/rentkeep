e11ff32b0c3306b2b4b99d4f60ccd8eab9bb4b83
Chase Greiner <chase.greiner@gmail.com>
2015-06-02 15:28:54 -0500

    Redesigned property list

---
+++ client/templates/properties/property_panel.js
@@ -0,0 +1,29 @@
+Template.propertyPanel.onCreated(function() {
+    //Initialization
+    var instance = this;
+
+    //Subscriptions
+    instance.subscribe('unitsByProperty', instance.data._id);
+
+    //Cursors
+    instance.units = function() {
+        return Units.find({propertyId: instance.data._id});
+    };
+
+});
+
+Template.propertyPanel.events({
+    'click tr': function(event) {
+        event.preventDefault();
+        Router.go('propertyView', this);
+    }
+});
+
+Template.propertyPanel.helpers({
+    units: function () {
+        return Template.instance().units();
+    },
+    singleUnitProperty: function() {
+        return Template.instance().units().count()===1;
+    }
+});
--- client/templates/properties/property_row.js
+++
@@ -1,6 +0,0 @@
-Template.propertyRow.events({
-    'click tr': function(event) {
-        event.preventDefault();
-        Router.go('propertyView', this);
-    }
-});
