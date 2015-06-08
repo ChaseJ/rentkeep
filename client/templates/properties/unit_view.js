8a5647b6ff503953e6d9109424c0581b71b1f39e
Chase Greiner <chase.greiner@gmail.com>
2015-06-07 21:30:25 -0500

    Added nav for unit view and different header if multiple properties

--- client/templates/properties/unit_view.js
+++ client/templates/properties/unit_view.js
@@ -4,11 +4,16 @@ Template.unitView.onCreated(function () {
     var unitId = Router.current().params._id;
     var propertyId = Router.current().params.propertyId;
 
+    instance.isMultipleUnits = new ReactiveVar(false);
+    Meteor.call('isMultipleUnits', propertyId, function(error,result){
+        instance.isMultipleUnits.set(result);
+    });
+
     //Subscriptions
     instance.subscribe('unit', unitId);
     instance.subscribe('property', propertyId);
 
-    //Cursors
+    //Data
     instance.unit = function() {
         return Units.findOne(unitId);
     };
@@ -23,5 +28,8 @@ Template.unitView.helpers({
     },
     property: function() {
         return Template.instance().property();
+    },
+    isMultipleUnits: function() {
+        return Template.instance().isMultipleUnits.get();
     }
 });
