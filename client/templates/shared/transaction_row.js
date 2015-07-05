c8dcfc3bbff06360d3c3ab9c5324c8eaa9f24a7b
Chase Greiner <chase.greiner@gmail.com>
2015-07-04 23:25:01 -0500

    Made transaction status a collection helper

--- client/templates/shared/transaction_row.js
+++ client/templates/shared/transaction_row.js
@@ -4,19 +4,3 @@ Template.transactionRow.events({
         Session.set('transactionId',Template.instance().data._id);
     }
 });
-
-Template.transactionRow.helpers({
-    status: function() {
-        if(this.balance > 0){
-            //Because moment() is local time, and dueDate is UTC midnight,
-            //I have to add the timezone offset to compare days
-            if(moment().isAfter(moment(this.dueDate).add(moment().zone(),"m"), 'day')){
-                return 'Late'
-            } else {
-                return 'Open'
-            }
-        } else {
-            return 'Paid'
-        }
-    }
-});
