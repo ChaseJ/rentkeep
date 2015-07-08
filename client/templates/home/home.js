8ba4c50445eff381e81221840f8100bbc40f5f85
Chase Greiner <chase.greiner@gmail.com>
2015-07-07 21:58:57 -0500

    Added template helpers for header squares data

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -25,13 +25,77 @@ Template.home.helpers({
         //If unit is vacant or if lease ends in next 30 days, return unit
         var unitArray = Template.instance().units().map(function(unit) {
             var currentLeaseEnds = unit.currentLeaseEnds();
+
             if( currentLeaseEnds === "Vacant") {
                 return unit;
-            } else if (moment(currentLeaseEnds).isBefore(moment().add(30,'d'))){
+            } else if (moment(currentLeaseEnds).isBefore(moment().add(1,'months'))){
                 return unit;
             }
         });
-        unitArray = _.compact(unitArray);
-        return unitArray;
+
+        return _.compact(unitArray);
+    },
+    rentPastDue: function() {
+        var rentPastDue = 0;
+        var today = moment();
+
+        Template.instance().transactions().forEach(function(transaction) {
+            //Because moment() is local time, and dueDate is UTC midnight,
+            //I have to add the timezone offset to compare days
+            var dueDate = moment(transaction.dueDate).subtract(moment().utcOffset(),"m");
+
+            if(today.isAfter(dueDate, 'day') && transaction.balance>0){
+                rentPastDue+=transaction.balance;
+            }
+        });
+
+        return rentPastDue;
+    },
+    rentMonthPastDue: function() {
+        var rentPastDue = 0;
+        var monthBeforeToday = moment().subtract(1, 'months');
+
+        Template.instance().transactions().forEach(function(transaction) {
+            var dueDate = moment(transaction.dueDate).subtract(moment().utcOffset(),"m");
+
+            if(monthBeforeToday.isAfter(dueDate, 'day')){
+                if(transaction.balance>0){
+                    rentPastDue = rentPastDue + transaction.balance;
+                }
+            }
+        });
+
+        return rentPastDue;
+    },
+    vacantUnits: function() {
+        var vacantUnits = 0;
+
+        Template.instance().units().forEach(function(unit) {
+            if( unit.currentLeaseEnds() === "Vacant") {
+                vacantUnits+=1;
+            }
+        });
+
+        return vacantUnits;
+    },
+    soonToBeVacantUnits: function() {
+        var soonToBeVacantUnits = 0;
+        var today = moment();
+        var monthFromToday = today.add(1, 'months');
+
+        Template.instance().units().forEach(function(unit) {
+            var currentLeaseEnds = moment(unit.currentLeaseEnds()).subtract(moment().utcOffset(),'m');
+            var nextLeaseStarts = moment(unit.nextLeaseStarts()).subtract(moment().utcOffset(),'m');
+
+            if(monthFromToday.isAfter(currentLeaseEnds, 'day')) {
+                if(!nextLeaseStarts.isValid()) {
+                    soonToBeVacantUnits+=1;
+                } else if (monthFromToday.isBefore(nextLeaseStarts, 'day')){
+                    soonToBeVacantUnits+=1;
+                }
+            }
+        });
+
+        return soonToBeVacantUnits;
     }
 });
