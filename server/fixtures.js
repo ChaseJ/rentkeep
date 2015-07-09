aa84ed60818c5dadcb1bd9da7eac431851315c26
Chase Greiner <chase.greiner@gmail.com>
2015-07-08 22:45:09 -0500

    Refactored fixtures

--- server/fixtures.js
+++ server/fixtures.js
@@ -13,44 +13,55 @@ if (Meteor.users.find().count() === 0) {
 
 //Methods to run on client to load test data
 Meteor.methods({
+    insertTestUserData: function() {
+        check(Meteor.user().emails[0].address, 'test@rentkeep.com');
+
+        Meteor.call('insertTestTenantData');
+        Meteor.call('insertTestPropertyData');
+    },
+    removeTestUserData: function() {
+        check(Meteor.user().emails[0].address, 'test@rentkeep.com');
+
+        var userId = Meteor.user()._id;
+
+        Properties.remove({userId: userId});
+        Units.remove({userId: userId});
+        Leases.remove({userId: userId});
+        Transactions.remove({userId: userId});
+        Documents.remove({userId: userId});
+        Tenants.remove({userId: userId});
+    },
     insertTestPropertyData: function(){
-        var unitsOne = ['1'];
-        var unitsTwo = ['100','200'];
-        var unitsFive = ['100', '101', '200', '201', '300'];
-        var unitsSeven = ['100', '110', '120', '200', '210', '300', '310'];
+        check(Meteor.user().emails[0].address, 'test@rentkeep.com');
 
         var properties = [
-            {street:'414 E Market St',      city:'Iowa City',       state:'IA',     postalCode:'52245',     units:unitsTwo},
-            {street:'509 S Jefferson St',   city:'Sigourney',       state:'IA',     postalCode:'52591',     units:unitsOne},
-            {street:'907 E Pleasant Valley',city:'Sigourney',       state:'IA',     postalCode:'52591',     units:unitsFive},
-            {street:'500 N Ellis St',       city:'Keota',           state:'IA',     postalCode:'52248',     units:unitsSeven},
-            {street:'25 Byington Road',     city:'Iowa City',       state:'IA',     postalCode:'52242',     units:unitsOne},
-            {street:'301 N Clinton St',     city:'Iowa City',       state:'IA',     postalCode:'52242',     units:unitsOne},
-            {street:'413 N Clinton St',     city:'Iowa City',       state:'IA',     postalCode:'52242',     units:unitsOne},
-            {street:'1110 N Dubuque St',    city:'Iowa City',       state:'IA',     postalCode:'52242',     units:unitsFive},
-            {street:'449 N Riverside Dr',   city:'Iowa City',       state:'IA',     postalCode:'52240',     units:unitsOne},
-            {street:'320 Grand Ave',        city:'Iowa City',       state:'IA',     postalCode:'52242',     units:unitsTwo},
-            {street:'245 Sugar Creek Ln',   city:'North Liberty',   state:'IA',     postalCode:'52317',     units:unitsTwo},
-            {street:'125 E Zeller St',      city:'North Liberty',   state:'IA',     postalCode:'52317',     units:unitsSeven}
+            {street:'414 E Market St',      city:'Iowa City',       state:'IA',     postalCode:'52245'},
+            {street:'509 S Jefferson St',   city:'Sigourney',       state:'IA',     postalCode:'52591'},
+            {street:'907 E Pleasant Valley',city:'Sigourney',       state:'IA',     postalCode:'52591'},
+            {street:'500 N Ellis St',       city:'Keota',           state:'IA',     postalCode:'52248'},
+            {street:'25 Byington Road',     city:'Iowa City',       state:'IA',     postalCode:'52242'},
+            {street:'301 N Clinton St',     city:'Iowa City',       state:'IA',     postalCode:'52242'},
+            {street:'413 N Clinton St',     city:'Iowa City',       state:'IA',     postalCode:'52242'},
+            {street:'1110 N Dubuque St',    city:'Iowa City',       state:'IA',     postalCode:'52242'},
+            {street:'449 N Riverside Dr',   city:'Iowa City',       state:'IA',     postalCode:'52240'},
+            {street:'320 Grand Ave',        city:'Iowa City',       state:'IA',     postalCode:'52242'},
+            {street:'245 Sugar Creek Ln',   city:'North Liberty',   state:'IA',     postalCode:'52317'},
+            {street:'125 E Zeller St',      city:'North Liberty',   state:'IA',     postalCode:'52317'}
+        ];
+        var units = [
+            [],
+            ['100','200'],
+            ['100', '101', '200', '201', '300'],
+            ['100', '110', '120', '200', '210', '300', '310']
         ];
 
-        for (var i=0; i<properties.length; i++){
-            var propertyId = Properties.insert({
-                street: properties[i].street,
-                city: properties[i].city,
-                state: properties[i].state,
-                postalCode: properties[i].postalCode
-            });
-
-            _.each(properties[i].units, function(unitNo){
-                Units.insert({
-                    propertyId: propertyId,
-                    unitNo: unitNo
-                })
-            })
-        }
+        _.each(properties, function(property) {
+            Meteor.call('propertyInsert', property, _.sample(units));
+        })
     },
     insertTestTenantData: function() {
+        check(Meteor.user().emails[0].address, 'test@rentkeep.com');
+
         var tenants = [
             {firstName:'John',      lastName:'Doe',         phone:'(555) 555-5555',    email:'john@example.com'},
             {firstName:'Steve',     lastName:'Johnson',     phone:'(555) 555-1234',    email:'steve@example.com'},
@@ -66,14 +77,9 @@ Meteor.methods({
             {firstName:'Sue',       lastName:'Peterson',    phone:'(555) 555-0002',    email:'sue@example.com'}
         ];
 
-        for (var i=0; i<tenants.length; i++){
-            Tenants.insert({
-                firstName: tenants[i].firstName,
-                lastName: tenants[i].lastName,
-                phone: tenants[i].phone,
-                email: tenants[i].email
-            });
-        }
+        _.each(tenants, function(tenant) {
+            Meteor.call('tenantInsert', tenant);
+        })
     }
 });
 
