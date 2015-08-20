9d2559ffd7ae6ed42dd3140ed307011771bbe514
Chase Greiner <chase.greiner@gmail.com>
2015-08-19 22:35:54 -0500

    Export tenants to csv

--- client/templates/tenants/tenants_list.js
+++ client/templates/tenants/tenants_list.js
@@ -9,8 +9,11 @@ Template.tenantsList.onCreated(function () {
     instance.subscribe('tenants');
 
     //Cursors
-    instance.tenants = function() {
+    instance.tenantsPagination = function() {
         return TenantsPagination.find({}, {sort: {lastName: 1}, itemsPerPage:10});
+    };
+    instance.tenants = function() {
+        return Tenants.find({}, {sort: {lastName: 1}});
     };
 });
 
@@ -20,8 +23,26 @@ Template.tenantsList.onRendered(function () {
     });
 });
 
+Template.tenantsList.events({
+    'click .export-btn': function(e) {
+        e.preventDefault();
+        var tenantsArray = Template.instance().tenants().map(function(tenant, index) {
+            //Show optional properties if not declared yet
+            if(index===0) {
+                if(!tenant.hasOwnProperty('phone')) {transaction.phone=''}
+                if(!tenant.hasOwnProperty('email')) {tenant.email=''}
+                if(!tenant.hasOwnProperty('notes')) {tenant.notes=''}
+            }
+            return _.omit(tenant, ['userId', "_id"]);
+        });
+        var csv = Papa.unparse(tenantsArray);
+        var blob = new Blob([csv], {type: "text/csv;charset=utf-8"});
+        saveAs(blob, "tenants.csv");
+    }
+});
+
 Template.tenantsList.helpers({
     tenants: function () {
-        return Template.instance().tenants();
+        return Template.instance().tenantsPagination();
     }
 });
