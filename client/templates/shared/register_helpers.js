81ad282b85c6d9ed9af10639a61ae25d50b2cee3
Chase Greiner <chase.greiner@gmail.com>
2015-09-18 10:47:06 -0500

    Formatted last emailed data

--- client/templates/shared/register_helpers.js
+++ client/templates/shared/register_helpers.js
@@ -6,6 +6,25 @@ Template.registerHelper("formatDate", function(value){
     return moment.isDate(value) ? moment.utc(value).format("M/D/YY") : value;
 });
 
+Template.registerHelper("formatDatetime", function(value){
+    //Check if browser supports toLocaleString
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleString
+    function toLocaleStringSupportsLocales() {
+        try {
+            new Date().toLocaleString('i');
+        } catch (e) {
+            return e.name === 'RangeError';
+        }
+        return false;
+    }
+
+    if(toLocaleStringSupportsLocales){
+        return moment(value).format("MMM Do YYYY, h:mm a") + ' ' + value.toLocaleString('en', {timeZoneName:'short'}).split(' ').pop();
+    } else {
+        return moment(value).format("MMM Do YYYY, h:mm a");
+    }
+});
+
 Template.registerHelper("formatCurrency", function(value){
     return numeral(value).format('$0,0[.]00');
 });
